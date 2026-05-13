d0eb99a36da25ecdaa9664d590efa86f0ce4bb07
Asadbek Alimov <asadbekalimov@Asadbeks-MacBook-Air.local>
2026-05-13 16:29:26 +0500

    about-us

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
-        const menuBtn = document.querySelector('.menu-btn');
-        const menuDropdown = document.querySelector('.menu-dropdown');
+const menuBtn = document.querySelector('.open-dropdownBtn');
+const menuDropdown = document.querySelector('.menu-dropdown');
 
-        menuBtn.addEventListener('click', (e) => {
-            e.stopPropagation();
-            menuDropdown.classList.toggle('active');
-        });
+menuBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    menuDropdown.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+});
 
-        document.addEventListener('click', (e) => {
-            if (!menuDropdown.contains(e.target) && !menuBtn.contains(e.target)) {
-                menuDropdown.classList.remove('active');
-            }
-        });
+document.addEventListener('click', (e) => {
+    if (!menuDropdown.contains(e.target) && !menuBtn.contains(e.target)) {
+        menuDropdown.classList.remove('active');
+        menuBtn.classList.remove('active');
+    }
+});
+
+
